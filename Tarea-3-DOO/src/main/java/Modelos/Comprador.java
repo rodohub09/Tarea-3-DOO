0eb2ee5d93b44e4c268acaa50501e0162ed40538
mihn <martin.henrnav@gmail.com>
2025-06-05 01:30:48 -0400

    Creacion de metodo ingresarMoneda para simplificar metodos de ingreso de distintas monedas.

--- Tarea-3-DOO/src/main/java/Modelos/Comprador.java
+++ Tarea-3-DOO/src/main/java/Modelos/Comprador.java
@@ -26,42 +26,24 @@ public class Comprador {
         inventario.add(producto);
     }
 
-    public void ingresarMoneda1000(Expendedor exp) throws PagoIncorrectoException {
-        Moneda mon = null;
-        for(Moneda m : billetera){
-            if(m.getValor() == 1000){
-                mon = m;
+    public void ingresarMoneda(Expendedor exp, int valor) throws PagoIncorrectoException {
+        for (Moneda m : billetera) {
+            if (m.getValor() == valor) {
+                exp.ingresarMoneda(m);
+                return;
             }
         }
-        if(mon==null){
-            return ;
-        }
-        exp.ingresarMoneda(mon);
+    }
+
+    public void ingresarMoneda1000(Expendedor exp) throws PagoIncorrectoException {
+        ingresarMoneda(exp, 1000);
     }
 
     public void ingresarMoneda500(Expendedor exp) throws PagoIncorrectoException {
-        Moneda mon = null;
-        for(Moneda m : billetera){
-            if(m.getValor() == 500){
-                mon = m;
-            }
-        }
-        if(mon==null){
-            return ;
-        }
-        exp.ingresarMoneda(mon);
+        ingresarMoneda(exp, 500);
     }
 
     public void ingresarMoneda100(Expendedor exp) throws PagoIncorrectoException {
-        Moneda mon = null;
-        for(Moneda m : billetera){
-            if(m.getValor() == 100){
-                mon = m;
-            }
-        }
-        if(mon==null){
-            return;
-        }
-        exp.ingresarMoneda(mon);
+        ingresarMoneda(exp, 100);
     }
 }
