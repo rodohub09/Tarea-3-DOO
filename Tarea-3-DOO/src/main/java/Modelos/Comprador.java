a8b09598b241764adc9e1c6158a3fe70eee28bdf
mihn <martin.henrnav@gmail.com>
2025-06-04 23:30:03 -0400

    Crear variables privadas billetera e inventario, y crear métodos tomarVuelto y tomarProducto con un enfoque aplicado a un comprador recogiendo el vuelto/producto del expendedor.

--- Tarea-3-DOO/src/main/java/Modelos/Comprador.java
+++ Tarea-3-DOO/src/main/java/Modelos/Comprador.java
@@ -1,57 +1,28 @@
 package Modelos;
+
+import java.util.ArrayList;
+
 /**
  * Clase que representara a quien interactua con el {@link Expendedor}, intentando comprar un {@link Producto}.
  * */
 
 public class Comprador {
 
-    /**
-     * Atributo que cuantifica el vuelto que ha acumulado, el cual siempre comienza en 0.
-     * */
-    private int vuelto = 0;
-    /**
-     * Atributo que indicara el producto consumido.
-     * */
-    private String producto;
+    private ArrayList<Moneda> billetera = new ArrayList<>();
+    private ArrayList<Producto> inventario;
 
-    /**
-     * Constructor
-     * @param exp El expendedor, del cual se le quiere comprar uno de sus {@link Productos}
-     * @param m {@link Moneda} con la cual la instancia pagara al comprar en el {@link Expendedor}.
-     * @param p Es lo que se quiere comprar dentro de los {@link Productos}.
-     * @throws PagoIncorrectoException Lanza esta excepcion, al entregar un <b>valor nulo</b> como moneda.
-     * @throws PagoInsuficienteException <p>Lanza esta excepción, al entregar una moneda con valor <b>menor</b> al precio
-     * del producto seleccionado.</p>
-     * @throws NoHayProductoException Lanza esta excepcion, al intentar comprar un producto el cual <b>ya no tiene stock.</b>
-     * */
-    public Comprador(Moneda m, Productos p, Expendedor exp) throws PagoIncorrectoException,PagoInsuficienteException,NoHayProductoException{
-        Producto product = exp.comprarProducto(m, p);
-        if (product != null)
-            producto = product.consumir();
-        else
-            producto = null;
+    public Comprador() {
 
-
-        Moneda mon;
-        while ((mon=exp.getVuelto()) != null){
-            vuelto += mon.getValor();
-        }
     }
 
-    /**
-     * Getter del vuelto que acumulo la instancia.
-     * @return Vuelto
-     * */
-    public int cuantoVuelto() {
-        return vuelto;
+    public void tomarVuelto(Expendedor exp) {
+        if (!exp.monVu.dep.isEmpty())
+            billetera.addAll(exp.monVu.getDep());
     }
 
-    /**
-     * Getter del "sabor" del producto, asi identificando que {@link Producto} de el {@link Expendedor} se ha consumido.
-     * @return {@link String} con el nombre del producto.
-     * */
-    public String queConsumiste(){
-        return producto;
-
+    public void tomarProducto(Expendedor exp) {
+        Producto producto =  exp.depositoProducto;
+        exp.depositoProducto = null;
+        inventario.add(producto);
     }
 }
